a21457773f135a0c420518ba5274b1af76320253
Adam Vail <adam.r.vail@gmail.com>
2013-03-31 21:13:03 -0700

    Basic start on creating a view of all the current rules in a switch's flowtable, need to create an alias class

---
+++ edu/wisc/cs/project/secure/Secure.java
@@ -0,0 +1,37 @@
+package edu.wisc.cs.project.secure;
+
+import java.util.List;
+
+import net.floodlightcontroller.core.IOFSwitch;
+
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.action.OFAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Secure {
+	
+	protected static Logger logger = LoggerFactory.getLogger(Secure.class);
+	
+	/**
+	 * This function is used in OFSwitchBase to check rules in the
+	 * write functions to make sure the switch should get the rule
+	 * @param rule - the rule to be written to the switch
+	 * @param sw - the switch that is trying to write the rule, this way
+	 * 				a view of the switch's current rules can be constructed
+	 * @return - true or false, if the rule is allowed to be written or not
+	 */
+	
+	public static boolean checkFlowRule(OFFlowMod rule, IOFSwitch sw){
+		
+		logger.debug("------------Secure just received a rule!---------------");
+		List<OFAction> actions = rule.getActions();
+		
+		for(OFAction action : actions){
+			logger.debug("Switch " + sw.getId() + ": " + action.toString());
+		}
+		
+		return true;
+	}
+
+}
